43567a621f794ac55aedabaa010b9161880672ad
Ryan on da cluster <rscherbarth@unm.edu>
2024-09-24 14:53:11 -0600

    some more broken things

--- scripts/setup-comp.sh
+++ scripts/setup-comp.sh
@@ -9,6 +9,7 @@
 echo -=-=-=-=-=-=-=-=-
 echo set ip addr
 echo -=-=-=-=-=-=-=-=-
+set -x
 
 # Update networking on computes
 # ifconfig eno8403 10.0.0.2/16 netmask 255.255.0.0
@@ -18,6 +19,11 @@ echo -=-=-=-=-=-=-=-=-
 #
 # ip route add default via 10.0.0.1 dev eno8403
 
+mkdir /opt/spack 
+chown root:systems /opt/spack
+mkdir /projects 
+chown root:systems /projects
+
 # Mounting filesystems 
 #
 # add to /etc/fstab
@@ -25,6 +31,9 @@ echo -=-=-=-=-=-=-=-=-
 # nfs mounts provided in warewulf.conf
 # 10.0.0.1:/home /home nfs defaults 0 0
 # 10.0.0.1:/projects /projects nfs defaults 0 0
+# 10.0.0.1:/opt/spack /opt/spack nfs defaults 0 0
+#
+mount -a
 #
 
 # change permissions
@@ -32,8 +41,11 @@ chmod u+x /usr
 chmod g+x /usr
 chmod o+x /usr
 
+chmod u+x /etc/slurm
+chmod g+x /etc/slurm
+chmod o+x /etc/slurm
+
 # turn things back on 
-set -x
 systemctl restart dbus
 systemctl restart home.mount
 systemctl restart rtkit-daemon
