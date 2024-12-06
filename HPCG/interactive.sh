5543fe2671c0f01cc2ffe3e98d3260ddfbc6585f
Ryan <104543985+rysc3@users.noreply.github.com>
2024-12-06 11:27:57 -0700

    Update interactive.sh

--- HPCG/interactive.sh
+++ HPCG/interactive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-export CONT='/ascldap/users/rdscher/nvidia/hpc-benchmarks:24.06.sif'
-# export CONT='/ascldap/users/rdscher/nvidia/hpc-benchmarks:24.03.sif'
+export CONT='path/to/hpc-benchmarks:24.06.sif'
+# export CONT='path/to/hpc-benchmarks:24.03.sif'
 export MOUNT="$PWD:/workspace/dat-files"
 
 echo "Running on hosts: $(echo $(scontrol show hostname))"
@@ -11,4 +11,3 @@ echo "-_-_-_-_-_-_-"
 echo "srun --mpi=pmi2 singularity run --nv -B $ MOUNT $ CONT ./hpl.sh --dat /workspace/dat-files/HPL.dat"
 echo "-_-_-_-_-_-_-"
 echo "-_-_-_-_-_-_-"
-
